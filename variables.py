133979b26922189c894db150ce6931e6d74a7ed5
keerti <sharmakeerti2302@gmail.com>
2021-12-05 17:53:32 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,17 @@
+import turtle
+import func
+a=5;
+print(a);
+b="hello world";
+print(b);
+wn=turtle.Screen()
+wn.bgcolor('light green')
+wn.title('Draw')
+tur=turtle.Turtle()
+tur.forward(100)
+tur.right(90)
+tur.forward(100)
+tur.right(90)
+tur.forward(100)
+tur.right(90)
+tur.forward(100)
