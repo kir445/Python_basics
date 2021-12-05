a705aaa0a140e6324140187bd0d77611a9298608
keerti <sharmakeerti2302@gmail.com>
2021-12-05 18:10:32 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,10 @@
+l={1,2,3,4}
+print(l)
+library_1={"Harry Potter","adolf hitler","13 reasons why"}
+library_2=("Harry Potter","HIMYM")
+li_2=set(library_2)
+
+li_2.add(4)
+print(li_2)
+school=library_1.union(li_2)
+print(school)
